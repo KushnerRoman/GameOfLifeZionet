490582d382838122fd7ae7dc4ef29ebac8944345
Roman Kushner <73998196+KushnerRoman@users.noreply.github.com>
2022-06-29 09:59:39 +0300

    Update GameOfLife.java

--- GameOfLife1d/src/main/java/main/GridGame/GameOfLife.java
+++ GameOfLife1d/src/main/java/main/GridGame/GameOfLife.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class GameOfLife extends JFrame {
 
